79a63865f2aea3a7faf70273977f5502b8b82f9b
ombhilare999 <ombhilare999@gmail.com>
2020-04-22 22:38:01 +0530

    Empty World Added

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/omkar/wroros_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/omkar/ros20_ws/devel;/opt/ros/kinetic".split(';') if "/home/omkar/ros20_ws/devel;/opt/ros/kinetic" != "" else []
